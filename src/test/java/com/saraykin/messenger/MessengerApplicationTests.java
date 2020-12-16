c13c03281b74afdef6c86918e98ada1eb65f3b91
v.saraikin <v.saraikin@infomaximum.com>
2020-12-16 15:37:58 +0300

    init

---
+++ src/test/java/com/saraykin/messenger/MessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraykin.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
